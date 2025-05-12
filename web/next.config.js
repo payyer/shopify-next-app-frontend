e2a612537575b10680ab4846b77986da495ebeb6
tisun1720 <tisun1720@gmail.com>
2025-05-12 15:35:12 +0700

    fix deploy

--- web/next.config.js
+++ web/next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: false,
   swcMinify: true,
   env: {
     NEXT_PUBLIC_HOST: process.env.HOST,
     NEXT_PUBLIC_SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
   },
-}
+  experimental: {
+    serverActions: true, // nếu bạn đang dùng App Router
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
